c944dd0761a0d9922e741e4acbaaaf8ab4a8de5d
kaiferal1 <40682570+kaiferal1@users.noreply.github.com>
2018-11-01 15:51:04 -0600

    Initial commit

---
+++ EisenmanProyect/EisenmanProyect/Servicios/Login.svc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EisenmanProyect.Servicios
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Login
+    {
+        // Para usar HTTP GET, agregue el atributo [WebGet]. (El valor predeterminado de ResponseFormat es WebMessageFormat.Json)
+        // Para crear una operación que devuelva XML,
+        //     agregue [WebGet(ResponseFormat=WebMessageFormat.Xml)]
+        //     e incluya la siguiente línea en el cuerpo de la operación:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [OperationContract]
+        public void DoWork()
+        {
+            // Agregue aquí la implementación de la operación
+            return;
+        }
+
+        // Agregue aquí más operaciones y márquelas con [OperationContract]
+    }
+}
